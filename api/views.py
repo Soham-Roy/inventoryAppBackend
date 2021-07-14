91ed5a9263a1cfed6391b90b0b0d82d3efdae7a8
soham <sohammusic38@gmail.com>
2021-07-14 19:46:31 +0530

    Updated views.py in api app

--- api/views.py
+++ api/views.py
@@ -17,3 +17,48 @@ def event_list(request):
     if request.method == "GET":
         serializer = EventSerializer(events, many = True)
         return Response(serializer.data, status=status.HTTP_200_OK)     
+
+@api_view(['PUT'])
+def update_event(request):
+    try:
+        event = Event.objects.get()
+    except Event.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "PUT":
+        serializer = EventSerializer(event, data= event.eventName)
+        data = {}
+        if ( serializer.is_valid ):
+            serializer.save()
+            data["operation"] = "update successful"
+            return Response(data= data)
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def delete_event(request):
+    try:
+        event = Event.objects.get()
+    except Event.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "DELETE":
+        operation = event.delete()
+        data = {}
+        if operation:
+            data["operation"] = "delete successful"
+        else:
+            data["operation"] = "delete failed"    
+        return Response(data= data) 
+
+@api_view(['POST'])
+def create_event(request):
+    event = Event()
+    if request.method == "PUT":
+        serializer = EventSerializer(event, data= event.eventName)
+        data = {}
+        if ( serializer.is_valid ):
+            serializer.save()
+            data["operation"] = "update successful"
+            return Response(data= data)
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
